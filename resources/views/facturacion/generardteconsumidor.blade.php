b967d0fbead2091549538292836d42a9baea684a
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-08-21 08:44:03 -0600

    cambios en factur

--- resources/views/facturacion/generardteconsumidor.blade.php
+++ resources/views/facturacion/generardteconsumidor.blade.php
@@ -120,7 +120,7 @@ function sacartotal($detalles){
 // Clases para estructurar el DTE
 class Identificacion {
     public $version = 1;
-    public $ambiente = "00";
+    public $ambiente = "01";
     public $tipoDte = "01"; 
     public $numeroControl;
     public $codigoGeneracion;
@@ -401,16 +401,16 @@ function enviarDTEAPI($dte) {
     $datos = [
         'Usuario' => "05090211591010",
         'Password' => "Santos25.",
-        'Ambiente' => '00',
+        'Ambiente' => '01',
         'DteJson' => json_encode($dte),
         'Nit' => "005207550",
-        'PasswordPrivado' => "20Xanadu25.",
+        'PasswordPrivado' => "25Xanadu20.",
         'TipoDte' => '01',
         'CodigoGeneracion' => $dte->identificacion->codigoGeneracion,
         'NumControl' => $dte->identificacion->numeroControl,
         'VersionDte' => 1,
-       // 'CorreoCliente' => "poncemarito2019@gmail.com"
-        'CorreoCliente' => "clientesfrecuentes02@gmail.com"
+        //'CorreoCliente' => "poncemarito2019@gmail.com"
+       'CorreoCliente' => "clientesfrecuentes02@gmail.com"
     ];
 
   //  echo "<pre>JSON generado:<br>" . json_encode($dte, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . "</pre>";
